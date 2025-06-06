b9f2942af592d76d6919149be8c97eabc619aae9
Jordan Butterfield <45950703+jmcbutter@users.noreply.github.com>
2025-06-06 02:06:10 -0500

    Create a file on google drive with appstat new

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
 # If modifying these scopes, delete the file token.json.
-SCOPES = ["https://www.googleapis.com/auth/drive.metadata.readonly"]
+SCOPES = ["https://www.googleapis.com/auth/drive", "https://www.googleapis.com/auth/docs", "https://www.googleapis.com/auth/documents", "https://www.googleapis.com/auth/spreadsheets"]
 
 
-def check_google_api():
-    """Shows basic usage of the Drive v3 API.
-    Prints the names and ids of the first 10 files the user has access to.
+def create_google_api_service():
+    """
+    Handles the google drive API calls
     """
     creds = None
     # The file token.json stores the user's access and refresh tokens, and is
@@ -37,28 +37,25 @@ def check_google_api():
 
     try:
         service = build("drive", "v3", credentials=creds)
-
-        # Call the Drive v3 API
-        results = (
-            service.files()
-            .list(pageSize=10, fields="nextPageToken, files(id, name)")
-            .execute()
-        )
-        items = results.get("files", [])
-
-        if not items:
-            print("No files found.")
-            return
-        print("Files:")
-        for item in items:
-            print(f"{item['name']} ({item['id']})")
+        return service
     except HttpError as error:
         # TODO(developer) - Handle errors from drive API.
         print(f"An error occurred: {error}")
 
 
 def create_new_application(args):
-    print(args.company, args.position)
+    service = create_google_api_service()
+    # TODO: Update so that the folder can be selected or can be placed in a config file
+    folder_id = '1uQLtAnUWl__FTCJ0qJcRghmSc-NGLAXr'
+
+    file_metadata = {"name": args.company + ' ' + args.position, "parents": [folder_id]}
+    # Call the Drive v3 API
+    file = (
+        service.files()
+        .create(body=file_metadata, fields="id, name")
+        .execute()
+    )
+    print(file.get("name"))
 
 
 def main():
@@ -74,8 +71,6 @@ def main():
 
     args = parser.parse_args()
     args.func(args)
-
-    check_google_api()
 
 
 if __name__ == '__main__':
